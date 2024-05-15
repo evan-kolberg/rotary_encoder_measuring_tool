d9925eed5415e4e0ef1cd1b0845a9d0d24aa5aea
Evan Kolberg <evankolberg@aol.com>
2024-05-14 20:58:31 -0400

    works plainly

--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,37 @@
 #include <Arduino.h>
 #include <LiquidCrystal_I2C.h>
 
-LiquidCrystal_I2C lcd(0x27, 16, 2);
-
 void PinA();
 void PinB();
 
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
 static int pinA = 2;
 static int pinB = 3;
-static int buttonPin = 4;
-volatile int encoderPos = 0;
-volatile int oldEncPos = 0;
 volatile byte aFlag = 0;
 volatile byte bFlag = 0;
+volatile byte encoderPos = 0;
+volatile byte oldEncPos = 0;
 volatile byte reading = 0;
-volatile bool recording = true;
-volatile bool showWheelSpecs = false;
-volatile bool wheelScaled = false;
 
 void setup() {
   pinMode(pinA, INPUT_PULLUP);
   pinMode(pinB, INPUT_PULLUP);
-  pinMode(buttonPin, INPUT_PULLUP);
   attachInterrupt(0,PinA,RISING);
   attachInterrupt(1,PinB,RISING);
+  Serial.begin(115200);
 
   lcd.init();
-  lcd.clear();
+  lcd.clear();         
   lcd.backlight();
-  lcd.setCursor(0, 0);
-  lcd.print("RECORD");
-
-  Serial.begin(115200);
+  
 }
 
 void PinA(){
   cli();
   reading = PIND & 0xC;
   if(reading == B00001100 && aFlag) {
-    if(encoderPos > 0) {
-      encoderPos --;
-    }
+    encoderPos --;
     bFlag = 0;
     aFlag = 0;
   }
@@ -61,43 +52,16 @@ void PinB(){
 }
 
 void loop(){
-  if(digitalRead(buttonPin) == LOW) {
-    delay(20); // debounce
-    if(digitalRead(buttonPin) == LOW) {
-      if(recording && encoderPos > 0) { // Check if any rotations have been recorded
-        recording = false;
-        wheelScaled = true;
-        lcd.clear();
-        lcd.setCursor(0, 0);
-        lcd.print("Recording Done");
-        delay(1000);
-      } else if(wheelScaled) {
-        showWheelSpecs = !showWheelSpecs;
-      }
-      while(digitalRead(buttonPin) == LOW); // wait for button release
-    }
-  }
+  if(oldEncPos != encoderPos) {
+    Serial.println(encoderPos);
 
-  if(!recording && oldEncPos != encoderPos && wheelScaled) {
     lcd.clear();
     lcd.setCursor(0, 0);
-    if(showWheelSpecs) {
-      float circumference = 1.0 / encoderPos; // 1 meter divided by number of rotations
-      float diameter = circumference / PI;
-      lcd.print("Diameter: ");
-      lcd.print(diameter);
-      lcd.print(" m");
-      lcd.setCursor(0, 1);
-      lcd.print("Circumference: ");
-      lcd.print(circumference);
-      lcd.print(" m");
-    } else {
-      lcd.print(encoderPos);
-      lcd.setCursor(0, 1);
-      lcd.print("Dist: " + String(encoderPos * PI));
-      lcd.setCursor(14, 1);
-      lcd.print("m");
-    }
+    lcd.print("Encoder: ");
+    lcd.print(encoderPos);
+
     oldEncPos = encoderPos;
   }
 }
+
+
